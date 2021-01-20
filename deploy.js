dbdd754eabd15cdf97eaef820aec09365aa32faa
Hayden Funk <hayden@ednas-MBP-2.attlocal.net>
2021-01-20 11:10:38 -0800

    updated IP

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.226.0.112',
+      host: '54.197.18.133',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
